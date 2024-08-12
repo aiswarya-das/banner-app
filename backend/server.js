db13d7626eca11b38a8af3306199147075491d89
aiswarya-das <aiswaryadas.a@gmail.com>
2024-08-13 00:43:54 +0530

    modified vercel json

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "yourpassword",
+  password: "Jithinammu1",
   database: "banner_db"
 });
 
