992ad9918afc8be468fa8321b1251ac822c98030
aiswarya-das <aiswaryadas.a@gmail.com>
2024-08-13 03:19:18 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -110,54 +110,64 @@
 // });
 
 // module.exports = app;
-require('dotenv').config();
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+// require('dotenv').config();
+// const express = require('express');
+// const mysql = require('mysql2');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+// const app = express();
+// app.use(cors());
+// app.use(bodyParser.json());
 
-// Create connection to MySQL
-console.log("host:",process.env.DB_HOST)
-console.log("db name:",process.env.DB_NAME)
-console.log("db user:",process.env.DB_USER)
-console.log("db PASS:",process.env.DB_PASSWORD)
-
-
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-// Connecting to MySQL
-db.connect(err => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
-  }
-  console.log('Connected to MySQL');
-});
+// // Create connection to MySQL
+// console.log("host:",process.env.DB_HOST)
+// console.log("db name:",process.env.DB_NAME)
+// console.log("db user:",process.env.DB_USER)
+// console.log("db PASS:",process.env.DB_PASSWORD)
 
 
-app.get("/", (req, res) => {
-  const sql = "SELECT * FROM banner_settings ORDER BY id DESC LIMIT 1";
-  db.query(sql, (err, data) => {
-    if (err) {
-      console.log("Error: ", err);
-      // return res.json(err);
-    }
-    console.log(data[0])
-    // return res.json(data[0]);
-    res.send("hello world");
-  });
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
+
+// // Connecting to MySQL
+// db.connect(err => {
+//   if (err) {
+//     console.error('Error connecting to MySQL:', err);
+//     return;
+//   }
+//   console.log('Connected to MySQL');
+// });
+
+
+// app.get("/", (req, res) => {
+//   const sql = "SELECT * FROM banner_settings ORDER BY id DESC LIMIT 1";
+//   db.query(sql, (err, data) => {
+//     if (err) {
+//       console.log("Error: ", err);
+//       // return res.json(err);
+//     }
+//     console.log(data[0])
+//     // return res.json(data[0]);
+//     res.send("hello world");
+//   });
   
-});
+// });
 
-app.listen(5001, () => console.log("Server ready on port 5000."));
+// app.listen(5001, () => console.log("Server ready on port 5000."));
+
+// module.exports = app;
+
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(5001, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
